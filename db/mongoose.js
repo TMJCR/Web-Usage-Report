ec267ad391b6a2d4d1e42dd1ad3190062e6d4b91
TMJCR <thomascrowdy@gmail.com>
2021-05-25 17:41:17 +0100

    Deployed Datatabse to Cloud

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb://TMJCR:QhMehCkdfJoH8N8d@cluster0-shard-00-00.tzcdh.mongodb.net:27017,cluster0-shard-00-01.tzcdh.mongodb.net:27017,cluster0-shard-00-02.tzcdh.mongodb.net:27017/web-usage-report?ssl=true&replicaSet=atlas-by3vyx-shard-0&authSource=admin&retryWrites=true&w=majority",
+  "mongodb",
+  // REDACTED
   {
     useNewUrlParser: true,
     useCreateIndex: true,
