0528149db51fd2aa1a75d6be59d56a5966307c31
TMJCR <thomascrowdy@gmail.com>
2021-06-10 16:28:39 +0100

    deploy

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,23 @@
-document.querySelectorAll('.downloadText').forEach((item) => {
+document.querySelectorAll(".downloadText").forEach((item) => {
   item.style.opacity = 1;
 });
 
-const monthList = document.getElementById('month-selection');
-const dayList = document.getElementById('day-selection');
+const monthList = document.getElementById("month-selection");
+const dayList = document.getElementById("day-selection");
 
 const febMonths = [30, 31];
 const aprMonths = [31];
 
 monthList.onchange = function () {
-  const selectedMonth = document.querySelector('#month-selection')
-    .selectedOptions[0].label;
-  const daySelection = document.querySelector('#day-selection').options;
+  const selectedMonth =
+    document.querySelector("#month-selection").selectedOptions[0].label;
+  const daySelection = document.querySelector("#day-selection").options;
   dayList.options.selectedIndex = 0;
-  if (selectedMonth === 'Feb') {
+  if (selectedMonth === "Feb") {
     febMonths.forEach((month) => {
       daySelection[month - 1].disabled = true;
     });
-  } else if (selectedMonth === 'Apr') {
+  } else if (selectedMonth === "Apr") {
     aprMonths.forEach((month) => {
       daySelection[month - 1].disabled = true;
     });
@@ -28,16 +28,16 @@ monthList.onchange = function () {
   }
 };
 
-const dateForm = document.querySelector('#date-form');
-dateForm.addEventListener('submit', (e) => {
+const dateForm = document.querySelector("#date-form");
+dateForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const day = document.querySelector('#day-selection').selectedOptions[0].value;
-  const month = document.querySelector('#month-selection').selectedOptions[0]
-    .value;
-  const year = document.querySelector('#year-selection').selectedOptions[0]
-    .value;
+  const day = document.querySelector("#day-selection").selectedOptions[0].value;
+  const month =
+    document.querySelector("#month-selection").selectedOptions[0].value;
+  const year =
+    document.querySelector("#year-selection").selectedOptions[0].value;
   fetch(
-    `http://localhost:3000/data/?month=${month}&day=${day}+&year=${year}`
+    `https://web-usage-report.herokuapp.com/data/?month=${month}&day=${day}+&year=${year}`
   ).then((response) => {
     response.json().then((data) => {
       if (data.error) {
@@ -49,8 +49,8 @@ dateForm.addEventListener('submit', (e) => {
 });
 
 const updateDownloadText = (data) => {
-  document.querySelectorAll('.downloadText').forEach((item) => {
-    const classToRemove = item.classList.value.split(' ').pop();
+  document.querySelectorAll(".downloadText").forEach((item) => {
+    const classToRemove = item.classList.value.split(" ").pop();
     item.classList.remove(classToRemove);
     item.classList.add(data.data.downloadColor);
     item.innerHTML = data.data.downloads[item.id];
@@ -58,16 +58,16 @@ const updateDownloadText = (data) => {
 };
 
 const updateValues = (data) => {
-  const updates = document.querySelectorAll('.update');
+  const updates = document.querySelectorAll(".update");
   for (update of updates) {
-    const item = update.id.split('-')[0];
+    const item = update.id.split("-")[0];
     update.innerText = data.data[item];
   }
 };
 
 const updateTop10List = (data, listClass, variableName) => {
   let resultIndex;
-  if (listClass.split('-')[1] === 'page') {
+  if (listClass.split("-")[1] === "page") {
     resultIndex = 0;
   } else {
     resultIndex = 1;
@@ -83,14 +83,14 @@ const updateTop10List = (data, listClass, variableName) => {
 };
 
 const updateTop10Image = (data) => {
-  const item = document.querySelectorAll('.company-image');
+  const item = document.querySelectorAll(".company-image");
   item.forEach((item, index) => {
     item.src = `../../public/img/${data.data.top10Companies[index][0]}.png`;
   });
 };
 
 const updateDisplayDate = (data) => {
-  const date = document.querySelector('#displayDate-update');
+  const date = document.querySelector("#displayDate-update");
   date.innerHTML = data.displayDate;
 };
 
@@ -103,16 +103,16 @@ const updateChartDataAndCharts = (data) => {
 };
 
 const updateAnimation = () => {
-  document.querySelectorAll('.shake').forEach((d) => {
-    d.classList.add('shaker');
+  document.querySelectorAll(".shake").forEach((d) => {
+    d.classList.add("shaker");
     setTimeout(() => {
-      d.classList.remove('shaker');
+      d.classList.remove("shaker");
     }, 1000);
   });
-  document.querySelectorAll('.shakeSide').forEach((d) => {
-    d.classList.add('shakerSide');
+  document.querySelectorAll(".shakeSide").forEach((d) => {
+    d.classList.add("shakerSide");
     setTimeout(() => {
-      d.classList.remove('shakerSide');
+      d.classList.remove("shakerSide");
     }, 1000);
   });
 };
@@ -121,10 +121,10 @@ const updateReport = (data) => {
   console.log(data);
   updateValues(data);
   updateDownloadText(data);
-  updateTop10List(data, '.popular-page', 'top10Pages');
-  updateTop10List(data, '.popular-value', 'top10Pages');
-  updateTop10List(data, '.company-page', 'top10Companies');
-  updateTop10List(data, '.company-value', 'top10Companies');
+  updateTop10List(data, ".popular-page", "top10Pages");
+  updateTop10List(data, ".popular-value", "top10Pages");
+  updateTop10List(data, ".company-page", "top10Companies");
+  updateTop10List(data, ".company-value", "top10Companies");
   updateTop10Image(data);
   updateDisplayDate(data);
   updateChartDataAndCharts(data);
