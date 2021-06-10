ee01e678197344d5df558817773e3fc3502700b7
TMJCR <thomascrowdy@gmail.com>
2021-06-10 18:05:36 +0100

    deploy

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb:", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb://TMJCR:QhMehCkdfJoH8N8d@cluster0-shard-00-00.tzcdh.mongodb.net:27017,cluster0-shard-00-01.tzcdh.mongodb.net:27017,cluster0-shard-00-02.tzcdh.mongodb.net:27017/web-usage-report?ssl=true&replicaSet=atlas-by3vyx-shard-0&authSource=admin&retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
